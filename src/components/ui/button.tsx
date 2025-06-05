5b4f613c08e65e98c20c46730faf9ac5272574e9
dymasyovie333 <yoviedymas@gmail.com>
2025-06-05 11:44:58 +0700

    Delete src directory

--- src/components/ui/button.tsx
+++
@@ -1,3 +0,0 @@
-export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} className={`bg-blue-600 text-white px-4 py-2 rounded ${props.className || ""}`}>{children}</button>;
-}
--- src/components/ui/input.tsx
+++
@@ -1,3 +0,0 @@
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={`border px-3 py-2 rounded w-full ${props.className || ""}`} />;
-}
